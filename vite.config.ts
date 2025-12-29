578c9c807ed31f24ab7b21bcfc746bb41e437d3a
Moon-Sangho <skym887@gmail.com>
2025-12-29 17:26:26 +0900

    chore: 테스팅 라이브러리 추가
    1. vitest
    - 테스트를 실행/감시하고, 결과를 리포팅하며, mock/spy 같은 기능 제공
    
    2. jsdom
    - Node 환경에는 원래 document, window 같은 브라우저 DOM API가 없음
    - React 컴포넌트는 렌더링 과정에서 DOM을 사용하므로, 테스트에서도 DOM이 필요
    - jsdom은 Node에서 돌아가는 “가짜 브라우저”로, DOM API를 상당 부분 구현
    
    3. @testing-library/react
    - React 컴포넌트를 테스트에서 렌더링할 수 있게 하고(render)
    - 화면에서 요소를 찾는 API를 제공(screen.getByRole, findByText 등)
    
    4. @testing-library/jest-dom
    - 기본 expect만으로는 DOM 검증 표현이 투박함.
    - 테스트가 '사람이 읽기 쉬운 문장' 형태가 되도록 도와주는 도구
    - 예시
     - toBeInTheDocument()
     - toHaveTextContent()
     - toBeDisabled()
     - toHaveAttribute()
     - toHaveAccessibleName()
    
    5. @testing-library/user-event
    - 클릭/타이핑/탭 이동 등을 사용자 수준으로 더 현실적으로 재현
     - 참고: https://www.daleseo.com/testing-library-user-agent/#user-event-%ED%8C%A8%ED%82%A
    
    6. MSW (Mock Service Worker)
    - 네트워크 요청을 가로채서 응답을 돌려주는 네트워크 레벨 모킹 툴

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -12,4 +12,12 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    // https://vitest.dev/config/#test-globals
+    globals: true,
+    css: true,
+    include: ["src/**/*.test.{ts,tsx}"],
+  },
 });
